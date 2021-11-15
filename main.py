4b52105b2c57a9d888a05c36a512e605aeecd4c9
MkAngelo <mikequi000@gmail.com>
2021-11-15 16:23:29 -0600

    validations_query

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python
 from typing import Optional
+from fastapi.param_functions import Query
 
 # Pydantic
 from pydantic import BaseModel
@@ -27,3 +28,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
