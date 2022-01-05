be6a5021bad5ebe6b21e4ae29217c6baed6a2f78
MkAngelo <mikequi000@gmail.com>
2022-01-04 19:36:55 -0600

    Files

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field, EmailStr, HttpUrl, PositiveInt
 
 # FastAPI
 from fastapi import status
-from fastapi import FastAPI, Body, Query, Path, Form, Header, Cookie
+from fastapi import FastAPI, Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -182,3 +182,17 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ): 
     return user_agent
+
+# Files
+
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return{
+        "Filename":image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
